3eb3cb22f6463ed3a778525a8000c0e0bed956b1
FloyzenCode <vasyplay323@gmail.com>
2025-11-28 23:20:10 +0400

    Create basic components for start

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Card.css'
+
+function Card() {
+  return (
+    <div>
+        <div className='bord'>
+            <p class="txt-card">Карточка</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ client/src/pages/Start/Start.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Start.css'
+import Card from '../../components/Card/Card'
+
+export default function Start() {
+  return (
+    <div className='start-page'>
+        <Card />
+    </div>
+  )
+}
